dd678c2743d2730fff77d4e600b89178cbce1f0c
Xavier Marquez <marquez.x@gmail.com>
2023-02-06 18:30:06 +1300

    Changed default partition to quicktest because parallel is in heavy use right now

--- _targets.R
+++ _targets.R
@@ -21,8 +21,8 @@ options(clustermq.scheduler = "multiprocess")
 
 # tar_make_future() configuration (okay to leave alone):
 future::plan(future.batchtools::batchtools_slurm, template = here::here("slurm.tmpl"),
-             resources = list(partition = "parallel", ncpus = 2,
-                              memory = "4G",
+             resources = list(partition = "quicktest", ncpus = 2,
+                              memory = "6G",
                               walltime = "1:00:00"))
 
 # Run the R scripts in the R/ folder with your custom functions:
