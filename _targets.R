708c31c55ccb7ce9da5e09fea30c9ff426930500
Xavier Marquez <marquez.x@gmail.com>
2023-02-07 21:48:38 +1300

    More memory, just for testing

--- _targets.R
+++ _targets.R
@@ -109,7 +109,7 @@ list(
     pattern = map(demagogue_samples),
     resources = tar_resources(future = tar_resources_future(
       plan = future::plan(future.batchtools::batchtools_slurm, template = "slurm.tmpl",
-                          resources = list(partition = "quicktest", memory = "5G", ncpus = 2,
+                          resources = list(partition = "quicktest", memory = "10G", ncpus = 4,
                                            walltime = "0:20:00"))))
   )
 
