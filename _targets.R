b570e61ca5cdfa50812a2c6cee8b4b414859ccb2
Xavier Marquez <marquez.x@gmail.com>
2023-02-08 05:18:22 +1300

    Template name

--- _targets.R
+++ _targets.R
@@ -20,7 +20,7 @@ tar_option_set(
 options(clustermq.scheduler = "multiprocess")
 
 # tar_make_future() configuration (okay to leave alone):
-future::plan(future.batchtools::batchtools_slurm, template = "slurm")
+future::plan(future.batchtools::batchtools_slurm, template = "batchtools.slurm.tmpl")
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
