820141fe245eee620f04bece5ad6efcad66e92bd
Xavier Marquez <marquez.x@gmail.com>
2023-02-08 04:01:55 +1300

    Once more into the breach

--- _targets.R
+++ _targets.R
@@ -110,7 +110,8 @@ list(
                                       cache_format = "rds"),
     pattern = map(demagogue_samples),
     resources = tar_resources(future = tar_resources_future(
-      plan = future::plan(future.batchtools::batchtools_slurm, template = "slurm.tmpl", .skip = FALSE),
+      plan = future::plan(future.batchtools::batchtools_slurm, template = "slurm.tmpl", .skip = FALSE,
+                          resources = list()),
       resources = list(partition = "quicktest", memory = "4G", ncpus = 4,
                        walltime = "0:40:00")))
     ),
@@ -120,7 +121,8 @@ list(
     command = compute_dfm(demagogue_files, cache_format = "rds"),
     pattern = map(demagogue_files),
     resources = tar_resources(future = tar_resources_future(
-      plan = future::plan(future.batchtools::batchtools_slurm, template = "slurm.tmpl", .skip = FALSE),
+      plan = future::plan(future.batchtools::batchtools_slurm, template = "slurm.tmpl", .skip = FALSE,
+                          resources = list()),
       resources = list(partition = "quicktest", memory = "8G", ncpus = 4,
                        walltime = "0:20:00"))),
     iteration = "list"
