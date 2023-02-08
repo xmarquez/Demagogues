2b9a40063f69afd35d79b9c155317a0c0ed9bb09
Xavier Marquez <marquez.x@gmail.com>
2023-02-08 14:00:04 +1300

    Changed pipe to magrittr style

--- R/modeling_functions.R
+++ R/modeling_functions.R
@@ -3,18 +3,18 @@ train_test_splits <-function(dfm, feat) {
 }
 
 train_test_splits.dictionary2 <- function(dfm, feat) {
-  df <- dfm |>
-    quanteda::dfm_lookup(feat) |>
-    quanteda::dfm_weight("boolean") |>
+  df <- dfm %>%
+    quanteda::dfm_lookup(feat) %>%
+    quanteda::dfm_weight("boolean") %>%
     quanteda::convert(to = "data.frame")
 
   rsample::initial_split(df, strata = names(feat))
 }
 
 train_test_splits.character <- function(dfm, feat) {
-  df <- dfm |>
-    quanteda::dfm_select(feat) |>
-    quanteda::dfm_weight("boolean") |>
+  df <- dfm %>%
+    quanteda::dfm_select(feat) %>%
+    quanteda::dfm_weight("boolean") %>%
     quanteda::convert(to = "data.frame")
 
   rsample::initial_split(df, strata = feat)
