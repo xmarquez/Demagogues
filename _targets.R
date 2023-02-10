b92cab7c558229e6ab6b9c68b9e09be5fa67a7a1
Xavier Marquez <marquez.x@gmail.com>
2023-02-10 23:13:58 +1300

    Changed something

--- _targets.R
+++ _targets.R
@@ -384,10 +384,10 @@ list(
       iteration = "list",
       resources = tar_resources(future = tar_resources_future(
         plan = future::tweak(future.batchtools::batchtools_slurm,
-                             resources = list(partition = "quicktest", memory = "20G", ncpus = 10,
-                                              walltime = "0:15:00")),
-        resources = list(partition = "quicktest", memory = "20G", ncpus = 10,
-                         walltime = "0:15:00")))
+                             resources = list(partition = "quicktest", memory = "10G", ncpus = 10,
+                                              walltime = "0:10:00")),
+        resources = list(partition = "quicktest", memory = "10G", ncpus = 10,
+                         walltime = "0:10:00"))),
 
     )
   ),
