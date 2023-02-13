3c67419eeb402bb6240c0ee1fa11a8d5ea298ed8
Xavier Marquez <marquez.x@gmail.com>
2023-02-13 19:14:41 +1300

    Code generator function (not used directly)

---
+++ R/code_generation.R
@@ -0,0 +1,52 @@
+rmd_blocks_graph_names <- function(graph_names) {
+
+  noun_blocks <- graph_names %>%
+    purrr::map(function(x) paste0("\n```{r fig-",
+                    x, "_nn}", "\n",
+                    "#| fig-cap: ",
+                    stringr::str_remove(x, "graph_") %>% stringr::str_replace_all("_", " "),
+                    ", nouns only. Terms at the top are more closely associated with 'democracy' in later years. Label indicates the peak density of association.", "\n",
+                    "#| fig-height: 7\n",
+                    "graph <- tar_read(",
+                    x, ")",
+                    "\n\n", "graph[[2]]", "\n\n",
+                    "```\n\n"
+    ))
+
+  verb_blocks <- graph_names %>%
+    purrr::map(function(x) paste0("\n```{r fig-",
+                    x, "_vb}", "\n",
+                    "#| fig-cap: ",
+                    stringr::str_remove(x, "graph_") %>% stringr::str_replace_all("_", " "),
+                    ", verbs only. Terms at the top are more closely associated with 'democracy' in later years. Label indicates the peak density of association.", "\n",
+                    "#| fig-height: 7\n",
+                    "\n\n", "graph[[3]]", "\n\n",
+                    "```\n\n"
+    ))
+
+  adjective_blocks <- graph_names %>%
+    purrr::map(function(x) paste0("\n```{r fig-",
+                    x, "_jj}", "\n",
+                    "#| fig-cap: ",
+                    stringr::str_remove(x, "graph_") %>% stringr::str_replace_all("_", " "),
+                    ", adjectives only. Terms at the top are more closely associated with 'democracy' in later years. Label indicates the peak density of association.", "\n",
+                    "#| fig-height: 7\n",
+                    "\n\n", "graph[[4]]", "\n\n",
+                    "```\n\n"
+    ))
+
+  ism_blocks <- graph_names %>%
+    purrr::map(function(x) paste0("\n```{r fig-",
+                    x, "_ism}", "\n",
+                    "#| fig-cap: ",
+                    stringr::str_remove(x, "graph_") %>% stringr::str_replace_all("_", " "),
+                    ", -ism terms only. Terms at the top are more closely associated with 'democracy' in later years. Label indicates the peak density of association.", "\n",
+                    "#| fig-height: 7\n",
+                    "\n\n", "graph[[5]]", "\n\n",
+                    "```\n\n"
+    ))
+
+  list(noun_blocks, verb_blocks, adjective_blocks, ism_blocks) %>%
+    purrr::pmap(paste) %>%
+    purrr::walk(cat)
+}
