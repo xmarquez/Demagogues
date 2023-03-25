75d65af671f3bdef935df2372aabd42bf2899eec
Xavier Marquez <marquez.x@gmail.com>
2023-03-25 12:58:34 -0400

    Added memory to run.sh so it can create Appendix.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --partition=parallel
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=52G
+#SBATCH --mem=54G
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=xavier.marquez@vuw.ac.nz
 
