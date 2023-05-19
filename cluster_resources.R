d5c22eb12ce4b41a551deb73d857534761f344f2
Xavier Marquez <marquez.x@gmail.com>
2023-05-18 22:52:57 -0400

    Moved to separate file for maintainability

---
+++ cluster_resources.R
@@ -0,0 +1,24 @@
+
+files_resources <- list(partition = "parallel", memory = "12G", ncpus = 2,
+                                  walltime = "3:00:00")
+
+dfm_resources <- list(partition = "parallel", memory = "45G", ncpus = 2,
+                      walltime = "2:00:00")
+
+splits_resources <- list(partition = "parallel", memory = "20G", ncpus = 2,
+                         walltime = "0:02:00")
+
+fcm_resources <- list(partition = "bigmem", memory = "128G", ncpus = 2,
+                      walltime = "0:40:00")
+
+predictive_model_resources <- list(partition = "parallel", memory = "50G",
+                                   ncpus = 2, walltime = "0:50:00")
+
+evaluation_model_resources <- list(partition = "parallel", memory = "15G",
+                                   ncpus = 2, walltime = "0:02:00")
+
+svd_word_vectors_resources <- list(partition = "parallel", memory = "25G", ncpus = 10,
+                                   walltime = "0:10:00")
+
+glove_word_vectors_resources <- list(partition = "parallel", memory = "1G", ncpus = 40,
+                                     walltime = "1:00:00")
