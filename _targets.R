2251cacc802580a40097a6b23b02a8cf4b1a656e
Xavier Marquez <marquez.x@gmail.com>
2023-05-19 15:10:45 -0400

    Partition trouble

--- _targets.R
+++ _targets.R
@@ -67,9 +67,9 @@ list(
     command = hathiTools::load_raw_hathifile(hathi_catalog),
     resources = tar_resources(future = tar_resources_future(
       plan = future::tweak(future.batchtools::batchtools_slurm,
-                           resources = list(partition = "quicktest", memory = "20G", ncpus = 12,
+                           resources = list(partition = "parallel", memory = "20G", ncpus = 12,
                                             walltime = "1:00:00")),
-      resources = list(partition = "quicktest", memory = "20G", ncpus = 12,
+      resources = list(partition = "parallel", memory = "20G", ncpus = 12,
                        walltime = "1:00:00")))
   ),
 
